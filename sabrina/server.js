942e9f908b91b8a87a193cd5182b446af9e22837
SABRINA TEE <sabrinatee@me.com>
2016-03-03 17:07:47 -0800

    Added auth routes compatible for client-side auth

--- sabrina/server.js
+++ sabrina/server.js
@@ -6,10 +6,11 @@ mongoose.connect(process.env.MONGOLAB_URI || 'mongodb://localhost/hogc_dev');
 const donorsRouter = require(__dirname + '/routes/donor_routes');
 const requestsRouter = require(__dirname + '/routes/request_routes');
 const authRouter = require(__dirname + '/routes/auth_routes');
+const userRouter = require(__dirname + '/routes/user_routes');
 
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', 'http://localhost:5000');
-  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, token');
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   next();
 });
@@ -17,6 +18,7 @@ app.use((req, res, next) => {
 app.use('/api', donorsRouter);
 app.use('/api', requestsRouter);
 app.use(authRouter);
+app.use('/api', userRouter);
 
 var PORT = process.env.PORT || 3000;
 var server = module.exports = exports = app.listen(PORT, () => console.log('Server started on port: ' + PORT));
